2dc7fa9fb3e5c5598ec4174a8f7b840b2e64547a
Mickaël Andrieu <mickael.andrieu@prestashop.com>
2019-01-15 01:02:00 +0100

    It now works as expected

--- src/ModuleInstaller.php
+++ src/ModuleInstaller.php
@@ -5,17 +5,65 @@ namespace PrestaShop\ComposerPlugin;
 use Composer\Installers\Installer;
 use Composer\Package\PackageInterface;
 use Composer\Repository\InstalledRepositoryInterface;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 final class ModuleInstaller extends Installer
 {
+    /**
+     * @var string the root vendor directory
+     */
+    private $rootVendorDir;
+
     /**
      * {@inheritdoc}
      */
     public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
     {
+        $this->rootVendorDir = $this->vendorDir;
+
         // doesn't change the behavior, sadly
-        $this->vendorDir = $this->getInstallPath($package). 'vendor';
+        $this->vendorDir = $this->getInstallPath($package) . 'vendor';
 
         parent::install($repo, $package);
+
+        $this->initializeVendorDir();
+
+        $this->installModuleDependencies();
+    }
+
+    /**
+     * Extract into another class: mimic calls to Composer.
+     */
+    public function installModuleDependencies()
+    {
+        $composerFile = $this->vendorDir . '/../composer.json';
+
+        if (file_exists($composerFile)) {
+            $process = new Process(
+                'composer install'
+            );
+            $process->setWorkingDirectory($this->vendorDir . '/../');
+            $process->setOptions(['suppress_errors' => false]);
+            $process->run();
+
+            if (!$process->isSuccessful()) {
+                throw new ProcessFailedException($process);
+            }
+
+            echo $process->getOutput();
+
+            $process = new Process(
+                'composer dump-autoload'
+            );
+            $process->setOptions(['suppress_errors' => false]);
+            $process->run();
+
+            if (!$process->isSuccessful()) {
+                throw new ProcessFailedException($process);
+            }
+
+            echo $process->getOutput();
+        }
     }
 }
--- src/PrestaShopModulePlugin.php
+++ src/PrestaShopModulePlugin.php
@@ -6,6 +6,10 @@ use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
+/**
+ * Install PrestaShop modules in "modules" folder and
+ * executes "composer install" in every module that have a composer.json file.
+ */
 final class PrestaShopModulePlugin implements PluginInterface
 {
     public function activate(Composer $composer, IOInterface $io)
